6639a6afc38a559d647c698a5b15965c37d59e7e
JiahuaZhang <hughzhanghz@gmail.com>
2020-05-11 22:08:30 -0400

    add basic

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { BorderOutlined, DeleteOutlined, CheckSquareOutlined } from '@ant-design/icons';
 
 function App() {
   const [todos, setTodos] = useState<{ description: string; completed: boolean }[]>([]);
@@ -18,9 +19,46 @@ function App() {
           }
         }}
       />
-      <ul>
+      <ul style={{ listStyle: 'none', padding: 0, margin: '0.5rem 2.5rem', fontSize: '2rem' }}>
         {todos.map((todo, index) => (
-          <li key={`${todo.description}-${index}`}>{todo.description}</li>
+          <li
+            key={`${todo.description}-${index}`}
+            style={{
+              borderBottom: '1px solid #2196f3',
+              display: 'grid',
+              gridTemplateColumns: 'max-content 1fr max-content',
+              alignItems: 'center',
+              color: todo.completed ? 'gray' : '',
+              marginBottom: '.8rem',
+              gap: '.25rem',
+            }}>
+            <span
+              style={{ cursor: 'pointer' }}
+              onClick={(event) => {
+                setTodos((todos) =>
+                  todos.map((todo, i) => {
+                    if (index === i) {
+                      return { ...todo, completed: !todo.completed };
+                    } else {
+                      return todo;
+                    }
+                  })
+                );
+              }}>
+              {todo.completed ? (
+                <CheckSquareOutlined style={{ color: 'green' }} />
+              ) : (
+                <BorderOutlined />
+              )}
+            </span>
+            {todo.description}
+            <DeleteOutlined
+              onClick={(event) => {
+                setTodos((todos) => todos.filter((_, i) => i !== index));
+              }}
+              style={{ color: '#ff5722', cursor: 'pointer' }}
+            />
+          </li>
         ))}
       </ul>
     </div>

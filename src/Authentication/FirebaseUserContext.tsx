9048d511f32a6cde9a9a2c26fb53441f78af9e6b
JiahuaZhang <hughzhanghz@gmail.com>
2020-07-24 22:02:12 -0400

    remove logs

--- src/Authentication/FirebaseUserContext.tsx
+++ src/Authentication/FirebaseUserContext.tsx
@@ -13,11 +13,7 @@ export const useFirebaseUser = () => {
   const [user, setUser] = useState({} as User);
 
   useEffect(() => {
-    console.log('starting to observing firebase auth');
     return firebase.auth().onAuthStateChanged((user) => {
-      console.group('onAuthStateChanged');
-      console.log(user);
-      console.groupEnd();
       if (user) {
         const { displayName, photoURL, uid } = user;
         setUser({

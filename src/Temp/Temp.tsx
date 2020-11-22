f15a05df52d98bd3880795c806baac83916b293a
Jiahua Zhang <JiaHua_Zhang@comcast.com>
2020-11-21 20:50:09 -0500

    clean

--- src/Temp/Temp.tsx
+++ src/Temp/Temp.tsx
@@ -1,12 +1,8 @@
 import React, { useState } from 'react';
-import { Refresh } from './Refresh';
-import { Example } from './Example';
 
 interface Props {}
 
 export const Temp = (props: Props) => {
-  const [count, setCount] = useState(0);
-
   return (
     <div
       style={
@@ -25,7 +21,6 @@ export const Temp = (props: Props) => {
         }
       }>
       {/* <Refresh onClick={() => setCount((c) => c + 1)} /> */}
-      <Example key={count} />
     </div>
   );
 };
